15b5ba2822dd2277a224d4d8c4b45f4d86e64b9e
Pr0solar-T1 <luis.jacobus@prosolarsolucoes.com.br>
2023-06-13 10:38:39 -0300

    segundo commit

--- teste4.py
+++ teste4.py
@@ -1,6 +1,5 @@
 import requests as r
 import pandas as pd
-
 import time as t
 # import json as j
 
@@ -11,12 +10,6 @@ tabela = pd.read_excel(arq, 'Planilha1')
 
 
 valores = {
-    'id':[],
-    'nome': [],
-    'specie': [],
-    'origin': [],
-}
-valores_att = {
     'id':[],
     'nome': [],
     'specie': [],
@@ -34,6 +27,12 @@ def buscar():
         if 'item' in data and len(data['item']) > 0:
             # print(len(data['item']))
             quant = 0
+            valores_atualizados = {
+                'id':[],
+                'nome': [],
+                'specie': [],
+                'origin': [],
+            }
 
             while quant < len(data['item']) - 1:
                 quant +=1
@@ -43,35 +42,65 @@ def buscar():
                 origin_item = data['item'][quant]['origin']['name']
                 # print(id_item,specie_item,origin_item)
 
+                
                 valores['id'].append(id_item)
                 valores['nome'].append(name_item)
                 valores['origin'].append(origin_item)
                 valores['specie'].append(specie_item)
 
+
+                # print(len(valores['id']),len(valores['nome']))
+
+                # valores_atualizados['id'].append(id_item)
+                # valores_atualizados['nome'].append(name_item)
+                # valores_atualizados
+
+                # for v in valores:
+                #     if valores[f'{v}'] == True:
+                #         print(valores)
+                
+
+
                 tabela_atualizada = tabela._append(pd.DataFrame(valores), ignore_index=True)
                 print(tabela_atualizada)
                 tabela_atualizada.to_excel('./planilhas/planilha6.xlsx')
-                
+
+
+def att():
+
+    valores['id'].clear()
+    valores['nome'].clear()
+    valores['origin'].clear()
+    valores['specie'].clear()
+
+    print(valores)
+    t.sleep(3)
+    buscar()
+
 
 buscar()
 
-
-
 continuar = input('Deseja atualizar a tabela? [s/n] ')
+
+while continuar != 's' and continuar != 'n':
+        print('Digite um valor valido!')
+        continuar = input('Deseja atualizar a tabela? [s/n] ')
 
 match continuar:
     case 's':
-        print(valores)
+        # print(valores)
+        
         print('processando...')
         t.sleep(5)
 
         while True:
-            buscar()
+            # buscar()
+            att()
             t.sleep(5)
 
 
 
     case 'n':
         print('ok')
-    case _:
-        print('Digite um valor valido!')
+
+        
